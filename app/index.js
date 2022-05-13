3400dfb38725139aae4d1c645f422a4024218da0
Andrea <andrea.bragante@studenti.unitn.it>
2022-05-13 02:13:39 +0200

    implemented connection to database

--- app/index.js
+++ app/index.js
@@ -1,12 +1,17 @@
-var express = require('express');
+/* IMPORTS */
+const express = require('express');
+const mongoose = require('mongoose')
+
 var app = express();
-app.use('/static', express.static('public'));
 PORT = 3000
 
-// Handling GET requests
-app.get('/', function(req, res){
-	res.send('Hello World!');
-});
+/* DATABASE CONNECTION */
+const dbConnection =  mongoose.connect(
+	'mongodb+srv://'+process.env.MONGODB_ADMIN+':'+MONGODB_PSW+'@cluster.i2lzz.mongodb.net/'+process.env.MONGODB_DB+'?retryWrites=true&w=majority',
+	{useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+		console.log('Database connected');
+})
+
 
 app.listen(PORT, function() {
 	console.log('Server running on port ', PORT);
