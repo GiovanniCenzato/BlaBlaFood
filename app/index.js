3f51e7cf371c12adbe9332112397d131a73a0a68
Andrea <andrea.bragante@studenti.unitn.it>
2022-05-13 02:31:54 +0200

    connectio to db and basic routing to api

--- app/index.js
+++ app/index.js
@@ -2,15 +2,35 @@
 const express = require('express');
 const mongoose = require('mongoose')
 
-var app = express();
+const users = require('./api/users.js')
+const announcements = require('./api/announcements.js')
+
+const app = express();
 PORT = 3000
 
+
 /* DATABASE CONNECTION */
-const dbConnection =  mongoose.connect(
-	'mongodb+srv://'+process.env.MONGODB_ADMIN+':'+MONGODB_PSW+'@cluster.i2lzz.mongodb.net/'+process.env.MONGODB_DB+'?retryWrites=true&w=majority',
-	{useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+const dbConnection =  mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+	.then(() => {
 		console.log('Database connected');
-})
+	}).catch(()=>{
+		console.log('There was an error connecting to the databse.')
+	})
+
+
+/* MIDDLEWARE CONFIG */
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', express.static('static'));
+
+
+/* ROUTING */
+app.use('/api/v1/users', users)
+app.use('/api/v1/announcements', announcements)
+
+/* logica di token checking */
+// TBI
 
 
 app.listen(PORT, function() {
