925a375d390c856b3643e91d1b11ac83c3abaf3d
Roberto <75741616+r-gheda@users.noreply.github.com>
2022-05-17 11:29:43 +0200

    commit: "added login api and token checking logic"

--- app/api/users.js
+++ app/api/users.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user.js');
+const User = require('../models/user')
 
+router.get('/me', async (req, res) => {
+    // check for token 
+    if (!req.loggedin) {
+        return res.status(403).json({
+            message: 'Error, user not logged in'
+        });
+    }
+    
+    // get user with email from request
+    let _user = await User.findOne({
+        email: req.loggedin.email
+    });
+
+    // send back infos as json
+    res.status(200).json({
+        message: 'User retrieved correctly',
+        user: _user
+    });
+})
 
 module.exports = router;
