a016892f361e9f78ab016fde2efcb3bfd36bfd17
Andrea <andrea.bragante@studenti.unitn.it>
2022-05-18 17:44:38 +0200

    updated announcement schema

--- app/models/announcement.js
+++ app/models/announcement.js
@@ -4,6 +4,8 @@ var Schema = mongoose.Schema;
 module.exports = mongoose.model('Announcement', new Schema({ 
 	title: String,
     description: String,
+    location: String,
+    date: Date,
     tags: [],
     authorId: Schema.Types.ObjectId,
     reservations: [],
