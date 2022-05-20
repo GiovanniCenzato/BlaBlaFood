ebe348bcc515195624006122195371be433d0011
Andrea <andrea.bragante@studenti.unitn.it>
2022-05-20 10:17:32 +0200

    merged with announcement handling, tested announcement retrieval

--- app/app.js
+++ app/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const users = require('./api/users');
 const announcements = require('./api/announcements,js');
+
 
 /**
  * Static HTML pages
@@ -11,6 +13,7 @@ app.use('', express.static('public'));
 /**
  * Express configuration
  */
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
