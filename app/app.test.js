7af713e83475aece32858e33804a3071ef192a2c
Giunsky <giulia.andreatta-2@studenti.unitn.it>
2022-05-31 13:23:16 +0200

    created app.test.js

---
+++ app/app.test.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+const app     = require('./app')
+
+test('app module should be defined', () => {
+    expect(app).toBeDefined();
+});
+
+test('GET / should return 200', () => {
+    return request(app).get('/').expect(200);
+});
